3e0fabd041ab14a9508c723bffc6e7ca13a9e4d4
khaliilii <khalili.mohsen@rocketmail.com>
2025-06-11 09:58:42 +0330

    Update userauthcenter_api.php

--- src/routes/userauthcenter_api.php
+++ src/routes/userauthcenter_api.php
@@ -3,29 +3,21 @@
 use App\Http\Controllers\UserAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-/*
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-*/
-Route::prefix('users')->group(function () {
-    //user sanctum
-    Route::middleware(['auth:sanctum'])->group(function () {
 
-    });
-    /***auth user chdeck/register/registerVerify/loginOtp/loginOtpVerify/login password */
-    Route::post('/check',[UserAuthController::class,'check']);
-    Route::post('/register',[UserAuthController::class,'register']);
-    Route::post('/register/verify',[UserAuthController::class,'registerVerify']);
-    Route::post('/login/otp',[UserAuthController::class,'logiByOtp']);
-    Route::post('/login/otp/verify',[UserAuthController::class,'logiByOtpVerify']);
-    Route::post('/login/password',[UserAuthController::class,'logiByPassword']);
 
-    /***update user profile(name/gender/avatar) and password */
-    /*
-    Route::post('update/{user_id}',[UserController::class],'userUpdate');
+Route::prefix('api')->middleware('api')->group(function () {
 
-    Route::resources([UserController::class]);
-    */
+	Route::prefix('users')->group(function () {
+	    Route::middleware(['auth:sanctum'])->group(function () {
+
+	    });
+
+		Route::post('check', [UserAuthController::class, 'check'])->name('user.check');
+	    Route::post('register', [UserAuthController::class, 'register'])->name('user.register');
+	    Route::post('register/verify', [UserAuthController::class, 'registerVerify'])->name('user.register.verify');
+	    Route::post('login/password', [UserAuthController::class, 'loginPassword'])->name('user.login.password');
+	    Route::post('login/otp', [UserAuthController::class, 'loginOtp'])->name('user.login.otp');
+	    Route::post('login/otp/verify', [UserAuthController::class, 'loginOtpVerify'])->name('user.login.otp.verify');
+	});
 
 });
