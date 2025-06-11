9ba743c4a095c7ae33032b3115ad1eeae7bb169a
khaliilii <khalili.mohsen@rocketmail.com>
2025-06-11 13:24:09 +0330

    Update userauthcenter_api.php

--- src/routes/userauthcenter_api.php
+++ src/routes/userauthcenter_api.php
@@ -15,9 +15,9 @@ Route::prefix('api')->middleware('api')->group(function () {
 		Route::post('check', [UserAuthController::class, 'check'])->name('user.check');
 	    Route::post('register', [UserAuthController::class, 'register'])->name('user.register');
 	    Route::post('register/verify', [UserAuthController::class, 'registerVerify'])->name('user.register.verify');
-	    Route::post('login/password', [UserAuthController::class, 'loginPassword'])->name('user.login.password');
-	    Route::post('login/otp', [UserAuthController::class, 'loginOtp'])->name('user.login.otp');
-	    Route::post('login/otp/verify', [UserAuthController::class, 'loginOtpVerify'])->name('user.login.otp.verify');
+	    Route::post('login/password', [UserAuthController::class, 'logiByPassword'])->name('user.login.password');
+	    Route::post('login/otp', [UserAuthController::class, 'logiByOtp'])->name('user.login.otp');
+	    Route::post('login/otp/verify', [UserAuthController::class, 'logiByOtpVerify'])->name('user.login.otp.verify');
 	});
 
 });
