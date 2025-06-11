a43cf8f6bc0a542d18c913a39614a984b04488c5
khaliilii <khalili.mohsen@rocketmail.com>
2025-06-11 13:26:50 +0330

    Update userauthcenter_api.php

--- src/routes/userauthcenter_api.php
+++ src/routes/userauthcenter_api.php
@@ -8,16 +8,17 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('api')->middleware('api')->group(function () {
 
 	Route::prefix('users')->group(function () {
-	    Route::middleware(['auth:sanctum'])->group(function () {
+	    	Route::middleware(['auth:sanctum'])->group(function () {
+			//routes by sanctum
 
-	    });
+	    	});
 
-	Route::post('check', [UserAuthController::class, 'check'])->name('user.check');
-	Route::post('register', [UserAuthController::class, 'register'])->name('user.register');
-	Route::post('register/verify', [UserAuthController::class, 'registerVerify'])->name('user.register.verify');
-	Route::post('login/password', [UserAuthController::class, 'logiByPassword'])->name('user.login.password');
-	Route::post('login/otp', [UserAuthController::class, 'logiByOtp'])->name('user.login.otp');
-	Route::post('login/otp/verify', [UserAuthController::class, 'logiByOtpVerify'])->name('user.login.otp.verify');
+		Route::post('check', [UserAuthController::class, 'check'])->name('user.check');
+		Route::post('register', [UserAuthController::class, 'register'])->name('user.register');
+		Route::post('register/verify', [UserAuthController::class, 'registerVerify'])->name('user.register.verify');
+		Route::post('login/password', [UserAuthController::class, 'logiByPassword'])->name('user.login.password');
+		Route::post('login/otp', [UserAuthController::class, 'logiByOtp'])->name('user.login.otp');
+		Route::post('login/otp/verify', [UserAuthController::class, 'logiByOtpVerify'])->name('user.login.otp.verify');
 		
 	});
 
