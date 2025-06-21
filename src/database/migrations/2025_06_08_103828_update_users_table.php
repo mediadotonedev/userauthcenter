3e72f84104bf95e036147f06d073a8e655a5ae4f
khaliilii <khalili.mohsen@rocketmail.com>
2025-06-21 22:24:48 +0330

    Update 2025_06_08_103828_update_users_table.php

--- src/database/migrations/2025_06_08_103828_update_users_table.php
+++ src/database/migrations/2025_06_08_103828_update_users_table.php
@@ -15,18 +15,19 @@ return new class extends Migration
             // افزودن ستون‌های جدید
             $table->string('avatar')->nullable()->comment('عکس پروفایل')->after('name');
             $table->string('nickname')->nullable()->comment('اسم مستعار')->after('avatar');
-            $table->string('phone')->index()->unique()->nullable()->comment('شماره موبایل')->after('nickname');
+            $table->string('phone')->index()->unique()->nullable()->comment('شماره موبایل')->after('name');
             $table->timestamp('phone_verified_at')->nullable()->comment('تاریخ تایید شماره موبایل')->after('phone');
             $table->timestamp('birth_date')->nullable()->comment('تاریخ تولد')->after('email_verified_at');
             $table->enum('gender', ['male', 'female'])->nullable()->comment('جنسیت')->after('birth_date');
             $table->bigInteger('fk_client_id')->unique()->nullable()->comment('آیدی کاربر در سیستم مرکزی');
-            $table->softDeletes()->comment('حذف نرم');
+            $table->softDeletes()->comment('softdelete');
 
             // اصلاح ستون‌های موجود
-            $table->string('name')->comment('نام کاربر')->nullable()->change();
+            $table->string('name')->comment('نام کاربر')->change();
             $table->string('email')->nullable()->index()->comment('ایمیل')->change();
             $table->string('password')->nullable()->comment('رمز عبور')->change();
             $table->timestamp('email_verified_at')->nullable()->comment('تاریخ تایید ایمیل')->change();
+
         });
     }
 
@@ -67,7 +68,7 @@ return new class extends Migration
             $table->dropSoftDeletes();
 
             // بازگرداندن ستون‌های اصلاح‌شده به حالت اولیه
-            $table->string('name')->comment(null)->nullable(false)->change();
+            $table->string('name')->comment(null)->change();
             $table->string('email')->index()->unique()->comment(null)->nullable(false)->change();
             $table->string('password')->comment(null)->change();
             $table->timestamp('email_verified_at')->nullable()->comment(null)->change();
